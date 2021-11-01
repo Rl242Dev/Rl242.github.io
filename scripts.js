8adaae732069a1042a681ae9c4a75108a989debe
Rl242Dev <74558778+Rl242Dev@users.noreply.github.com>
2021-11-01 13:07:53 +0100

    Translate for page404

---
+++ scripts.js
@@ -0,0 +1,13 @@
+function TranslateHome() {
+    
+}
+function Translate404() {
+    document.getElementById('title').innerHTML = 'Page not Found - Error 404';
+    document.getElementById('ERT').innerHTML = 'Error 404!';
+    document.getElementById('p').innerHTML = 'An Error Occured, Page not found';
+}
+function Translate404fr() {
+    document.getElementById('title').innerHTML = 'Page non trouveé - Erreur 404';
+    document.getElementById('ERT').innerHTML = 'Erreur 404!';
+    document.getElementById('p').innerHTML = 'La page n\'a pas été trouvée';
+}
