d031f89a78aafd431ddd7a3aec82de906070511a
Rl242Dev <robin.lachand@gmail.com>
2021-11-01 18:40:44 +0100

    Start Translate Index X Problem Color Letter When click Button

--- scripts.js
+++ scripts.js
@@ -11,3 +11,13 @@ function Translate404fr() {
     document.getElementById('ERT').innerHTML = 'Erreur 404!';
     document.getElementById('p').innerHTML = 'La page n\'a pas été trouvée';
 }
+function Translateindexfr() {
+    document.getElementById('title').innerHTML = 'Rl242Dev - Apprenti Developpeur Web et Back-End';
+    document.getElementById('Cite').innerHTML = 'Votre site seul ou ensemble ?';
+    document.getElementById('liP').innerHTML = 'Projets';
+    document.getElementById('liC').innerHTML = 'Contact';
+    document.getElementById('StartIAM').innerHTML = 'Mon nom est Robin';
+    document.getElementById('AFSTIAM').innerHTML = 'Je suis un';
+    document.getElementById('What').innerHTML = 'Apprenti Web & Back-End Dévelloppeur';
+    
+}
