097e7dc423a47cd39c1d0a4bc750faa21c559613
Rl242Dev <74558778+Rl242Dev@users.noreply.github.com>
2021-11-02 11:59:11 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,3 @@
-function TranslateHome() {
-    
-}
 function Translate404() {
     document.getElementById('title').innerHTML = 'Page not Found - Error 404';
     document.getElementById('ERT').innerHTML = 'Error 404!';
@@ -11,13 +8,7 @@ function Translate404fr() {
     document.getElementById('ERT').innerHTML = 'Erreur 404!';
     document.getElementById('p').innerHTML = 'La page n\'a pas été trouvée';
 }
-function Translateindexfr() {
-    document.getElementById('title').innerHTML = 'Rl242Dev - Apprenti Developpeur Web et Back-End';
-    document.getElementById('Cite').innerHTML = 'Votre site seul ou ensemble ?';
-    document.getElementById('liP').innerHTML = 'Projets';
-    document.getElementById('liC').innerHTML = 'Contact';
-    document.getElementById('StartIAM').innerHTML = 'Mon nom est Robin';
-    document.getElementById('AFSTIAM').innerHTML = 'Je suis un';
-    document.getElementById('What').innerHTML = 'Apprenti Web & Back-End Dévelloppeur';
-    
-}
+/* Only Translate 404 because :
+1. Simple since not a lot of Text
+2. Because in "Index.html" & "Projets.html" There's some letters with special Style Code that cannot be done with Javascript or i just Don't know how to do it
+*/
