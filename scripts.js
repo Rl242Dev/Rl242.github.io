dc657db460cbe378bf7ab35442138c266506c3f0
Rl242Dev <74558778+Rl242Dev@users.noreply.github.com>
2021-11-03 21:40:32 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -8,7 +8,19 @@ function Translate404fr() {
     document.getElementById('ERT').innerHTML = 'Erreur 404!';
     document.getElementById('p').innerHTML = 'La page n\'a pas été trouvée';
 }
-/* Only Translate 404 because :
+function TranslateContact() {
+    document.getElementById('Why').innerHTML = 'Why ?';
+    document.getElementById('Fhint').innerHTML = 'Need any help for a Project ?';
+    document.getElementById('Shint').innerHTML = 'Any ideas of project ?';
+    document.getElementById('ButtonForm').innerHTML = 'Send';
+}
+function TranslateContactfr() {
+    document.getElementById('Why').innerHTML = 'Pourquoi ?';
+    document.getElementById('Fhint').innerHTML = 'Besoin d\'aide pour un projet ?';
+    document.getElementById('Shint').innerHTML = 'Des idées de projets ?';
+    document.getElementById('ButtonForm').innerHTML = 'Envoyer';
+}
+/* Only Translate 404 And Contact because :
 1. Simple since not a lot of Text
 2. Because in "Index.html" & "Projets.html" There's some letters with special Style Code that cannot be done with Javascript or i just Don't know how to do it
 */
